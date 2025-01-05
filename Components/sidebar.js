4ba97a64cfba8f076b69b774cf63e498c10b0fbf
amgaland <Amgalan.dbb@gmail.com>
2025-01-05 20:06:25 +0800

    sidebar complete

--- Components/sidebar.js
+++ Components/sidebar.js
@@ -1,33 +1,30 @@
 <template id="sidebar-menu-component">
   <nav class="menu">
-    <a href="#" class="menu-item">
+    <a href="./Dashboard.html" class="menu-item">
       Dashboard
     </a>
-    <a href="#" class="menu-item">
+    <a href="./MatchMaking.html" class="menu-item">
       Match Making
     </a>
-    <a href="#" class="menu-item">
+    <a href="./Activities.html" class="menu-item">
       Activities
     </a>
-    <a href="#" class="menu-item">
+    <a href="./Event.html" class="menu-item">
       Event
     </a>
-    <a href="#" class="menu-item">
+    <a href="./Notification.html" class="menu-item">
       Notification
     </a>
-    <a href="#" class="menu-item">
+    <a href="./clubs.html" class="menu-item">
       Clubs
     </a>
-    <a href="#" class="menu-item">
-      Basketball
-    </a>
-    <a href="#" class="menu-item">
+    <a href="./CreateClub.html" class="menu-item">
       Create a club
     </a>
-    <a href="#" class="menu-item">
+    <a href="./Settings.html" class="menu-item">
       Settings
     </a>
-    <a href="#" class="menu-item upgrade">
+    <a href="./Upgrade.html" class="menu-item upgrade">
       Upgrade
     </a>
   </nav>
